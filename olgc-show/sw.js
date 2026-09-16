cf35255629a42f442fd8ea7d4f7e4056c3f98ae0
jonnybode <jonnybode@gmail.com>
2026-09-16 20:44:22 +1000

    Adapt cue notes for caller with music-start and entrance timing

--- olgc-show/sw.js
+++ olgc-show/sw.js
@@ -1,4 +1,4 @@
-const CACHE='olgc-show-v2-roles-20260916-4';
+const CACHE='olgc-show-v2-caller-20260916-5';
 const ASSETS=['./','./index.html','./v2.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(ASSETS.map(async path=>{
